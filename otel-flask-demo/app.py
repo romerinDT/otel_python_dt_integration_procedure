2a685d06a68072ced8ccd358a9530cffac578036
romerinDT <128828020+romerinDT@users.noreply.github.com>
2023-10-04 09:28:20 -0500

    Update app.py

--- otel-flask-demo/app.py
+++ otel-flask-demo/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 # Datos en memoria
 DATA = []
 
-SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
+SWAGGER_URL = '/api-docs'
+API_URL = 'swagger.json'
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
