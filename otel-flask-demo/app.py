b1abe8e5974cbce60a58d0efdd814a782de44919
romerinDT <128828020+romerinDT@users.noreply.github.com>
2023-10-04 09:58:08 -0500

    Update app.py

--- otel-flask-demo/app.py
+++ otel-flask-demo/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+
 from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
@@ -6,8 +7,10 @@ app = Flask(__name__)
 # Datos en memoria
 DATA = []
 
+# Swagger UI Configuration
 SWAGGER_URL = '/api-docs'
-API_URL = 'swagger.json'
+API_URL = '/swagger.json'  # Nota el cambio aquí: añadimos un slash al principio
+
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
@@ -26,6 +29,12 @@ def add_data():
     item = request.json
     DATA.append(item)
     return jsonify({"message": "Data added successfully!"}), 201
+
+@app.route('/swagger.json')  # Ruta para servir el archivo swagger.json
+def swagger_json():
+    with open('swagger.json', 'r') as f:
+        json_content = f.read()
+    return json_content, 200, {'Content-Type': 'application/json'}
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
